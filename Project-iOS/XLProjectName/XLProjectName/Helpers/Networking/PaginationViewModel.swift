ee7a7af38610040612ecef30abfdc3a202bf9256
Diego Medina <diego.medina@xmartlabs.com>
2016-04-04 13:16:12 -0300

    Code refactor

--- Project-iOS/XLProjectName/XLProjectName/Helpers/Networking/PaginationViewModel.swift
+++ Project-iOS/XLProjectName/XLProjectName/Helpers/Networking/PaginationViewModel.swift
@@ -22,7 +22,8 @@ class PaginationViewModel<Element: Decodable where Element.DecodedType == Elemen
     let queryTrigger = PublishSubject<String>()
     let filterTrigger = PublishSubject<FilterType>()
     let networkErrorTrigger = PublishSubject<NetworkError>()
-
+    
+    let queryPending = Variable<Bool>(false)
     let hasNextPage = Variable<Bool>(false)
     let fullloading = Variable<LoadingType>((false, "1"))
     let elements = Variable<[Element]>([])
@@ -39,10 +40,12 @@ class PaginationViewModel<Element: Decodable where Element.DecodedType == Elemen
     private func setUpForceRefresh() {
         
         queryTrigger
+            .doOnNext { [weak self] _ in self?.queryPending.value = true }
             .throttle(0.25, scheduler: MainScheduler.instance)
             .distinctUntilChanged()
             .doOnNext { [weak self] queryString in
                 guard let mySelf = self else { return }
+                mySelf.queryPending.value = false
                 mySelf.bindPaginationRequest(mySelf.paginationRequest.routeWithQuery(queryString), nextPage: nil)
             }
             .map { _ in false }
